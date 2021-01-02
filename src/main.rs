97a11f3d80c034d1fc0f02fccede99ec7f83869d
Carl Hurd <carl@basilisklabs.com>
2021-01-01 22:42:04 -0500

    Added Custom Emoji support that is really ugly, needs to be refactored

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,6 @@ impl Handler {
                                 if let Some(user_id) = user_id {
                                     // Use the `emoji` Unicode for comparison
                                     match emoji {
-                                        // We don't support custom server emoji's because I am lazy
                                         ReactionType::Unicode(emoji) => {
                                             // Parse the role list by taking any text the reactions are attached to
                                             // then get the last "\n\n" split.
@@ -131,9 +130,97 @@ impl Handler {
                                                     )
                                                 }
                                             }
-                                        }
+                                        },
+                                        // This variant occurs when you just absolutely need a custom emoji in your 
+                                        // channel because all the possible Unicode emoji's are not enough.
+                                        ReactionType::Custom {id, name, ..} => {
+                                            let name = if let Some(name) = name {
+                                                name
+                                            } else {
+                                                println!("Custom emoji name is None?");
+                                                return
+                                            };
+
+                                            // Parse the role list by taking any text the reactions are attached to
+                                            // then get the last "\n\n" split.
+                                            let role_list = match msg.content.split("\n\n").last() {
+                                                Some(role_list) => role_list,
+                                                None => {
+                                                    println!("Error while parsing the pinned message, couldn't find role list");
+                                                    return;
+                                                }
+                                            };
+
+                                            let mut role = "";
+
+                                            // Parse the `role_list` to get the specific role by iterating over each line
+                                            // comparing to see if the unicode matches, then spliting on `:`, trim the last
+                                            // split, which we assume is our role name
+                                            role_list.lines().filter(|line| line.contains(&name)).for_each(|line| {
+                                                role = match line.split(":").last() {
+                                                    Some(role_split) => role_split.trim(),
+                                                    None => {
+                                                        println!("Error while parsing the pinned message, couldn't find role within {}", line);
+                                                        return
+                                                    }
+                                                };
+                                            });
+
+                                            // We get the `Member` of the `GuildId` so that we can assign or remove the role
+                                            let mut member = guild_id
+                                                .member(ctx.http.clone(), user_id)
+                                                .await
+                                                .unwrap();
+
+                                            // We need to get the `RoleId` by turning our `GuildId` into a `PartialGuild`
+                                            // such that we can call `role_by_name` just so that we don't need to track
+                                            // the rediculous ID's associated with each role
+                                            let role_value = match guild_id
+                                                .to_partial_guild(ctx.http.clone())
+                                                .await
+                                            {
+                                                Ok(pg) => {
+                                                    if let Some(role) = pg.role_by_name(role) {
+                                                        role.id
+                                                    } else {
+                                                        println!(
+                                                            "Error while getting role by name"
+                                                        );
+                                                        return;
+                                                    }
+                                                }
+                                                Err(e) => {
+                                                    println!(
+                                                        "Error {} while getting PartialGuild",
+                                                        e
+                                                    );
+                                                    return;
+                                                }
+                                            };
+
+                                            // Finally, we take the associated action with that Role, adding it
+                                            // or removing it from the particular member
+                                            match action {
+                                                Action::AddRole => {
+                                                    println!(
+                                                        "AddRole for member : {:?}",
+                                                        member
+                                                            .add_role(ctx.http.clone(), role_value)
+                                                            .await
+                                                    );
+                                                }
+                                                Action::RemoveRole => {
+                                                    println!(
+                                                        "RemoveRole for member : {:?}",
+                                                        member
+                                                            .remove_role(ctx.http.clone(), role_value)
+                                                            .await
+                                                    )
+                                                }
+                                            }
+                                        },
                                         _ => {
-                                            println!("Server custom emoji's are not supported");
+                                            println!("Unsupported ReactionType!");
                                         }
                                     }
                                 }
